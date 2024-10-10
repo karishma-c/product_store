be4698077534499fbd2a4247df640769cb6c235a
karishma-c <karishmadgct@gmail.com>
2024-10-10 20:42:16 +0530

    created a db file for database connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 //create a express app
 const app = express();
+
+//get request and response
+app.get("/products", (req,res) => {})
+
+//db connection check
+console.log(process.env.MONGO_URI);
 
 //app listen to the port 
 app.listen(5000, () => {
