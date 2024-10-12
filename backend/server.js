39fe429dbb466028306ccb63e91eddfac0df1ebd
karishma-c <karishmadgct@gmail.com>
2024-10-12 17:50:44 +0530

    post request created with success and failure condition

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -8,7 +9,22 @@ dotenv.config();
 const app = express();
 
 //get request and response
-app.get("/products", (req,res) => {})
+app.post("/products", async (req,res) => {
+    const product = req.body; //getting the product
+
+    if(!product.name || !product.price || !product.image) { //checking for requirements
+        return res.status(400).json({success: false, message: "Please provide all fields"})
+    }
+
+    const newProduct = new Product(product) //Product is coming from product.model.js, product is from the req.body
+    try {
+        await newProduct.save(); //creating and saving the new product in db
+        res.status(201).json({success: true, data: newProduct}) //status 201 -> something is created
+    } catch (error) {
+        console.error("Error in Create Product:", error.message);
+        res.status(500).json({success: false, message: "Server Error"}) //status 500 -> internal server error
+    }
+})
 
 //db connection check
 //console.log(process.env.MONGO_URI);
