5b751ee41f95e59a7c12f1e95b31926d2e33fd7c
karishma-c <karishmadgct@gmail.com>
2024-10-18 20:56:57 +0530

    added global store to get all the products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import { Container, Text, VStack } from '@chakra-ui/react'
+import React, { useEffect } from 'react';
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { useProductStore } from '../store/product';
 
 const HomePage = () => {
+
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products)
+
   return (
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
@@ -15,6 +24,18 @@ const HomePage = () => {
         >
         Current Products
         </Text>
+
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3
+          }}
+          spacing={10}
+          w={"full"}
+        >
+
+        </SimpleGrid>
 
         <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
           No Products found
