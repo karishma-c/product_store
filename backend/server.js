9dd4c29862b8af196f6f330260afb04527090ae0
karishma-c <karishmadgct@gmail.com>
2024-10-19 21:11:00 +0530

    updated server.js and package.json to run the code based on development and production environment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"; //built-in node module
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -7,14 +8,23 @@ dotenv.config();
 
 //create a express app
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 //allows to accept JSON data in req.body(middleware)
 app.use(express.json());
 
 //function to get the routes(endpoints)
-
 app.use("/api/products", productRoutes); //the endpoint in the routes files should be prefixed with -> /api/products for eg: /api/products/ or /api/products/id 
+
+//to check the environment(production or development)
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 //app listen to the port 
 app.listen(PORT, () => {
