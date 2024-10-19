f534ecaa538d34e6a35f3fbdcabacfb7b2042d02
karishma-c <karishmadgct@gmail.com>
2024-10-19 19:32:12 +0530

    created delete product function

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,14 +40,17 @@ const HomePage = () => {
           ))}
         </SimpleGrid>
 
-        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
-          No Products found
-          <Link to={"/create"}>
-            <Text as='span' color="blue.500" _hover={{ textDecoration: "underline" }}>
-              Create a Product
-            </Text>
-          </Link>
-        </Text>
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+            No Products found
+            <Link to={"/create"}>
+              <Text as='span' color="blue.500" _hover={{ textDecoration: "underline" }}>
+                Create a Product
+              </Text>
+            </Link>
+          </Text>
+        )}
+        
       </VStack>
     </Container>
   )
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -24,5 +24,15 @@ export const useProductStore = create((set) => ({
 		const res = await fetch("/api/products");
 		const data = await res.json();
 		set({ products: data.data });
+	},
+	deleteProduct: async (pid) => {
+		const res = await fetch(`/api/products/${pid}`, {
+			method: "DELETE",
+		});
+		const data = await res.json();
+		if(!data.success) return { success: false, message: data.message };
+		//update the ui immediately, without this filter the product gets deleted but the ui get updated inly after we refresh the page
+		set(state => ({ products: state.products.filter(product => product._id !== pid) }));
+		return { success: true, message: data.message };
 	}
 }))
