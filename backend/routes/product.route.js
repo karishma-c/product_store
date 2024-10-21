100f1896bcf0b7eb314586b8dae2bf065c40612c
Karishma C <59170306+karishma-c@users.noreply.github.com>
2024-10-21 21:14:49 +0530

    Update product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -8,15 +8,15 @@ app.get("/", (req,res) => {
 })
 
 //endpoint to get all the products
-router.get("/", getProducts)
+router.get("https://project-backend-sage.vercel.app/", getProducts)
 
 //get request and response(endpoint to get a product)
-router.post("/", createProduct)
+router.post("https://project-backend-sage.vercel.app/", createProduct)
 
 //endpoint to update a product
-router.put("/:id", updateProduct) //we can use put as well as patch both are same, put is used to update all fields, patch is used to update some of the fields
+router.put("https://project-backend-sage.vercel.app/:id", updateProduct) //we can use put as well as patch both are same, put is used to update all fields, patch is used to update some of the fields
 
 //endpoint to delete a product
-router.delete('/:id', deleteProduct) //id is dynamic(unique id generated from the api request)
+router.delete('https://project-backend-sage.vercel.app/:id', deleteProduct) //id is dynamic(unique id generated from the api request)
 
 export default router;
