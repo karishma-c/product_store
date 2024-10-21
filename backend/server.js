883264bcd69fb2f5e2556434ec8df25ea298dfa6
Karishma C <59170306+karishma-c@users.noreply.github.com>
2024-10-21 21:06:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 
 app.use(cors(
     {
-        origin: [],
+        origin: ["https://project-frontend-sandy.vercel.app/"],
         method: ["POST", "GET"],
         credentials: true
     }
